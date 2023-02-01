567e796a741cc54ce4a3510c470e40dda0a6cc19
Dulat Krymov <dulat.krymov@jits-4-13.jits.ipa.dc>
2023-02-01 18:43:36 +0600

    Added chips, but selector does not work

--- app/src/main/java/kz/kd/hw_105/FragmentSearch.kt
+++ app/src/main/java/kz/kd/hw_105/FragmentSearch.kt
@@ -1,6 +1,11 @@
 package kz.kd.hw_105
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 
 class FragmentSearch : Fragment(R.layout.fragment_search) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 }
