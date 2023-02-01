64eaa523d4585ffe83acdf4b558bb4ac441faf05
krymov.dulat <krymov.dulat@gmail.com>
2023-02-01 14:28:33 +0600

    Copy/Past swipe and drag functions, item decorators, menu from HW16.11

--- app/src/main/java/kz/kd/hw_105/FragmentConvertor.kt
+++ app/src/main/java/kz/kd/hw_105/FragmentConvertor.kt
@@ -2,23 +2,51 @@ package kz.kd.hw_105
 
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+
+private const val KEY_VISIBLE_ITEM = "Visible Item"
+private const val KEY_COMPLETELY_VISIBLE_ITEM = "Completely Visible Item"
 
 class FragmentConvertor : Fragment(R.layout.fragment_convertor) {
 
     private lateinit var currencyAdapter: CurrencyAdapter
     private lateinit var currencyLayoutManager: LinearLayoutManager
     private lateinit var rvCurrency: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //Does not work
+        if (savedInstanceState != null) {
+            val firstVisibleItem = savedInstanceState.getInt(KEY_VISIBLE_ITEM)
+            val firstCompletelyVisibleItem = savedInstanceState.getInt(KEY_COMPLETELY_VISIBLE_ITEM)
+            currencyLayoutManager.scrollToPositionWithOffset(
+                firstVisibleItem,
+                firstCompletelyVisibleItem
+            )
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         rvCurrency = view.findViewById(R.id.rv_currency)
         initCurrencyRecycler()
+        fillCurrency()
+    }
+
+    //Does not work
+    override fun onSaveInstanceState(outState: Bundle) {
+        val firstVisibleItem = currencyLayoutManager.findFirstVisibleItemPosition()
+        val firstCompletelyVisibleItem =
+            currencyLayoutManager.findFirstCompletelyVisibleItemPosition()
+        outState.putInt(KEY_VISIBLE_ITEM, firstVisibleItem)
+        outState.putInt(KEY_COMPLETELY_VISIBLE_ITEM, firstCompletelyVisibleItem)
+        super.onSaveInstanceState(outState)
     }
 
     private fun initCurrencyRecycler() {
@@ -31,5 +59,86 @@ class FragmentConvertor : Fragment(R.layout.fragment_convertor) {
         currencyLayoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
         rvCurrency.layoutManager = currencyLayoutManager
         rvCurrency.adapter = currencyAdapter
+
+        val dividerItemDecoration =
+            DividerItemDecoration(context, currencyLayoutManager.orientation)
+        dividerItemDecoration.setDrawable(
+            context?.let {
+                AppCompatResources.getDrawable(
+                    it,
+                    R.drawable.divider_bg
+                )
+            }!!
+        )
+        rvCurrency.addItemDecoration(dividerItemDecoration)
+
+        swipeCurrency()
+    }
+
+    private fun swipeCurrency() {
+        val secondActivity = activity as SecondActivity?
+        val touchHelper =
+            ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
+                ItemTouchHelper.UP or ItemTouchHelper.DOWN,
+                ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+            ) {
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    target: RecyclerView.ViewHolder
+                ): Boolean {
+                    currencyAdapter.onMoveDrag(viewHolder.layoutPosition, target.layoutPosition)
+                    secondActivity?.tbConvertorDeleteChangeToConvertor()
+                    return true
+                }
+
+                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                    currencyAdapter.onSwipeDelete(viewHolder.layoutPosition)
+                    secondActivity?.tbConvertorDeleteChangeToConvertor()
+                }
+
+                override fun isLongPressDragEnabled(): Boolean {
+                    (activity as SecondActivity?)?.tbConvertorChangeToConvertorDelete()
+                    return super.isLongPressDragEnabled()
+                }
+            })
+        touchHelper.attachToRecyclerView(rvCurrency)
+    }
+
+    private fun fillCurrency() {
+        val currencyList = mutableListOf<Currency>()
+        currencyList.add(
+            Currency(
+                amount = "1",
+                flag = R.drawable.ic_kz,
+                country = getString(R.string.kz),
+                currencyName = getString(R.string.kz_currency)
+            )
+        )
+        currencyList.add(
+            Currency(
+                amount = "2",
+                flag = R.drawable.ic_usa,
+                country = getString(R.string.usa),
+                currencyName = getString(R.string.usa_currency)
+            )
+        )
+        currencyList.add(
+            Currency(
+                amount = "3",
+                flag = R.drawable.ic_tr,
+                country = getString(R.string.tr),
+                currencyName = getString(R.string.tr_currency)
+            )
+        )
+        currencyList.add(
+            Currency(
+                amount = "4",
+                flag = R.drawable.ic_eu,
+                country = getString(R.string.eu),
+                currencyName = getString(R.string.eu_currency)
+            )
+        )
+        currencyAdapter.updateDataSet(currencyList)
     }
 }
--- app/src/main/java/kz/kd/hw_105/SecondActivity.kt
+++ app/src/main/java/kz/kd/hw_105/SecondActivity.kt
@@ -1,6 +1,9 @@
 package kz.kd.hw_105
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.isVisible
@@ -14,6 +17,7 @@ class SecondActivity : AppCompatActivity(R.layout.activity_second) {
     private lateinit var tbConvertorDelete: Toolbar
     private lateinit var tbSearch: Toolbar
     private lateinit var tbAccount: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -28,6 +32,42 @@ class SecondActivity : AppCompatActivity(R.layout.activity_second) {
         tbConvertorDelete = findViewById(R.id.tb_convertor_delete)
         tbSearch = findViewById(R.id.tb_search)
         tbAccount = findViewById(R.id.tb_account)
+        setToolBarVisibility(0)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        if (tbConvertor.isVisible) {
+            inflater.inflate(R.menu.menu_tb_convertor, menu)
+        } else if (tbConvertorDelete.isVisible) {
+            inflater.inflate(R.menu.menu_tb_convertor_delete, menu)
+        }
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_main_reset -> {
+//                currencyAdapter.reset()
+                invalidateOptionsMenu()
+                true
+            }
+            R.id.menu_main_sort_alpha -> {
+                item.isChecked = !item.isChecked
+//                currencyAdapter.sortAlpha()
+                true
+            }
+            R.id.menu_main_sort_num -> {
+                item.isChecked = !item.isChecked
+//                currencyAdapter.sortNum()
+                true
+            }
+            R.id.menu_delete -> {
+//                DFDelete().show(supportFragmentManager, null)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     private fun initBottomNavigationBar() {
@@ -35,69 +75,117 @@ class SecondActivity : AppCompatActivity(R.layout.activity_second) {
         bnvSecondActivity.setOnItemSelectedListener {
             when (it.itemId) {
                 R.id.menu_bnv_chat -> {
-                    tbChat.isVisible = true
-                    tbFavorites.isVisible = false
-                    tbConvertor.isVisible = false
-                    tbConvertorDelete.isVisible = false
-                    tbSearch.isVisible = false
-                    tbAccount.isVisible = false
+                    setToolBarVisibility(1)
                     supportFragmentManager.beginTransaction()
-                        .replace(R.id.fl_second_activity, FragmentChat(), "Fragment Chat").commit()
+                        .replace(R.id.fl_second_activity, FragmentChat(), "Fragment Chat")
+                        .commit()
                     true
                 }
                 R.id.menu_bnv_favorites -> {
-                    tbChat.isVisible = false
-                    tbFavorites.isVisible = true
-                    tbConvertor.isVisible = false
-                    tbConvertorDelete.isVisible = false
-                    tbSearch.isVisible = false
-                    tbAccount.isVisible = false
-                    setSupportActionBar(tbFavorites)
+                    setToolBarVisibility(2)
                     supportFragmentManager.beginTransaction()
-                        .replace(R.id.fl_second_activity, FragmentFavorites(), "Fragment Chat")
+                        .replace(R.id.fl_second_activity, FragmentFavorites(), "Fragment Favorites")
                         .commit()
-
                     true
                 }
                 R.id.menu_bnv_convertor -> {
-                    tbChat.isVisible = false
-                    tbFavorites.isVisible = false
-                    tbConvertor.isVisible = true
-                    tbConvertorDelete.isVisible = false
-                    tbSearch.isVisible = false
-                    tbAccount.isVisible = false
+                    setToolBarVisibility(3)
                     supportFragmentManager.beginTransaction()
                         .replace(R.id.fl_second_activity, FragmentConvertor(), "Fragment Convertor")
                         .commit()
                     true
                 }
                 R.id.menu_bnv_search -> {
-                    tbChat.isVisible = false
-                    tbFavorites.isVisible = false
-                    tbConvertor.isVisible = false
-                    tbConvertorDelete.isVisible = false
-                    tbSearch.isVisible = true
-                    tbAccount.isVisible = false
+                    setToolBarVisibility(4)
                     supportFragmentManager.beginTransaction()
-                        .replace(R.id.fl_second_activity, FragmentSearch(), "Fragment Chat")
+                        .replace(R.id.fl_second_activity, FragmentSearch(), "Fragment Search")
                         .commit()
-
                     true
                 }
                 R.id.menu_bnv_account -> {
-                    tbChat.isVisible = false
-                    tbFavorites.isVisible = false
-                    tbConvertor.isVisible = false
-                    tbConvertorDelete.isVisible = false
-                    tbSearch.isVisible = false
-                    tbAccount.isVisible = true
+                    setToolBarVisibility(5)
                     supportFragmentManager.beginTransaction()
-                        .replace(R.id.fl_second_activity, FragmentAccount(), "Fragment Chat")
+                        .replace(R.id.fl_second_activity, FragmentAccount(), "Fragment Account")
                         .commit()
                     true
                 }
                 else -> false
             }
         }
+    }
+
+    private fun setToolBarVisibility(type: Int) {
+        when (type) {
+            0 -> {
+                tbChat.isVisible = false
+                tbFavorites.isVisible = false
+                tbConvertor.isVisible = false
+                tbConvertorDelete.isVisible = false
+                tbSearch.isVisible = false
+                tbAccount.isVisible = false
+            }
+            1 -> {
+                tbChat.isVisible = true
+                tbFavorites.isVisible = false
+                tbConvertor.isVisible = false
+                tbConvertorDelete.isVisible = false
+                tbSearch.isVisible = false
+                tbAccount.isVisible = false
+                setSupportActionBar(tbChat)
+            }
+            2 -> {
+                tbChat.isVisible = false
+                tbFavorites.isVisible = true
+                tbConvertor.isVisible = false
+                tbConvertorDelete.isVisible = false
+                tbSearch.isVisible = false
+                tbAccount.isVisible = false
+                setSupportActionBar(tbFavorites)
+            }
+            3 -> {
+                tbChat.isVisible = false
+                tbFavorites.isVisible = false
+                tbConvertor.isVisible = true
+                tbConvertorDelete.isVisible = false
+                tbSearch.isVisible = false
+                tbAccount.isVisible = false
+                setSupportActionBar(tbConvertor)
+                tbConvertor.overflowIcon?.setTint(resources.getColor(R.color.btn_text_color))
+            }
+            4 -> {
+                tbChat.isVisible = false
+                tbFavorites.isVisible = false
+                tbConvertor.isVisible = false
+                tbConvertorDelete.isVisible = false
+                tbSearch.isVisible = true
+                tbAccount.isVisible = false
+                setSupportActionBar(tbSearch)
+            }
+            5 -> {
+                tbChat.isVisible = false
+                tbFavorites.isVisible = false
+                tbConvertor.isVisible = false
+                tbConvertorDelete.isVisible = false
+                tbSearch.isVisible = false
+                tbAccount.isVisible = true
+                setSupportActionBar(tbAccount)
+            }
+        }
+    }
+
+    fun tbConvertorChangeToConvertorDelete() {
+        tbConvertor.isVisible = false
+        tbConvertorDelete.isVisible = true
+        setSupportActionBar(tbConvertorDelete)
+    }
+
+    fun tbConvertorDeleteChangeToConvertor() {
+        tbConvertor.isVisible = true
+        tbConvertorDelete.isVisible = false
+    }
+
+    override fun onBackPressed() {
+        tbConvertorDeleteChangeToConvertor()
+        super.onBackPressed()
     }
 }
