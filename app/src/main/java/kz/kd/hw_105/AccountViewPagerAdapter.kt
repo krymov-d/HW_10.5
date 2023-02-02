879b1aa8c0a2936b70c420c02372ec98f807dac3
krymov.dulat <krymov.dulat@gmail.com>
2023-02-02 15:42:04 +0600

    Added View Pager on Fragment Account

---
+++ app/src/main/java/kz/kd/hw_105/AccountViewPagerAdapter.kt
@@ -0,0 +1,34 @@
+package kz.kd.hw_105
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager.widget.PagerAdapter
+
+class AccountViewPagerAdapter : PagerAdapter() {
+
+    private val layouts = listOf(
+        R.layout.vp_account_tab_main,
+        R.layout.vp_account_tab_statistics,
+        R.layout.vp_account_tab_another
+    )
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val inflater = LayoutInflater.from(container.context)
+        val layout = inflater.inflate(layouts[position], container, false) as ViewGroup
+        container.addView(layout)
+        return layout
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        container.removeView(`object` as View?)
+    }
+
+    override fun getCount(): Int {
+        return layouts.size
+    }
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view === `object`
+    }
+}
--- app/src/main/java/kz/kd/hw_105/FragmentAccount.kt
+++ app/src/main/java/kz/kd/hw_105/FragmentAccount.kt
@@ -1,6 +1,26 @@
 package kz.kd.hw_105
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class FragmentAccount : Fragment(R.layout.fragment_account) {
+
+    private lateinit var vpAccount: ViewPager
+    private lateinit var vpTabLayout: TabLayout
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViewPager(view)
+    }
+
+    private fun initViewPager(view: View) {
+        vpAccount = view.findViewById(R.id.account_view_pager)
+        val vpAccountAdapter = AccountViewPagerAdapter()
+        vpAccount.adapter = vpAccountAdapter
+        vpAccount.currentItem = 0
+//        vpTabLayout.setupWithViewPager(vpAccount)
+    }
 }
